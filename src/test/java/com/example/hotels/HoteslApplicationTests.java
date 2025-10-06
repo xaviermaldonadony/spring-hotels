ac7970de683b8020f98cbd4cfe1b20813e8ad7ba
Xavier <Xaviermaldonadony@hotmail.com>
2025-10-05 20:05:46 -0400

    initialized hotels project

---
+++ src/test/java/com/example/hotels/HoteslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoteslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
